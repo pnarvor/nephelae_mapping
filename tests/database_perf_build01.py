33824e9ac0afb01a7ed45de0a7f24aaad9d504c5
Pierre Narvor <pnarvor@laas.fr>
2019-07-16 15:34:35 +0200

    [database/tests] Fixed displayed values for database performance test (no further insert optimisation required)

--- tests/database_perf_build01.py
+++ tests/database_perf_build01.py
@@ -6,12 +6,14 @@ import os
 import signal
 import time
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 from ivy.std_api import *
 import logging
 
 import nephelae_paparazzi.pprzinterface as ppint
-from nephelae_paparazzi.pprzinterface.messages import *
+import nephelae_paparazzi.pprzinterface.messages as pmsg
 from nephelae_base.types import *
 from nephelae_mapping.database import SpatializedDatabase
 from nephelae_mapping.database import NephelaeDataServer
@@ -21,7 +23,7 @@ from helpers.helpers import *
 
 dtbase = NephelaeDataServer()
 # dtbase.enable_periodic_save('output/database_perf01.neph', 60.0, True)
-dtbase.set_navigation_frame(NavigationRef("100 NAVIGATION_REF 0.0 0.0 31 0.0"))
+dtbase.set_navigation_frame(pmsg.NavigationRef("100 NAVIGATION_REF 0.0 0.0 31 0.0"))
 dtbase.navFrame.stamp = 0.0
 
 uavIds   = ['100', '101', '102', '103', '104']
@@ -29,6 +31,7 @@ varNames = ['var0', 'var1', 'var2', 'var3', 'var4']
 gpsSig  = 2000.0
 dataSig = 10.0
 N = 5*3600
+t = []
 try:
     t0 = time.time()
     for n in range(N):
@@ -36,14 +39,15 @@ try:
         if int(100 * n / N) == 100.0*n / N:
             t1 = time.time()
             print("Filling database... ("+str(int(100*n/N))+"% : "+str(n)+"/"+str(N)+")")
-            print(" - ", format(1000.0*(t1 - t0) / (len(uavIds)*len(varNames)), ".2f"),
-                  "ms per insert. (total : ", n, "inserted)")
+            print(" - ", format(1000000.0*(t1 - t0) / (len(uavIds)*(len(varNames) + 1)*N) * 100.0, ".2f"),
+                  "us per insert. (total : ", n*(len(uavIds)*(len(varNames) + 1)), "inserted)")
+            t.append(1000000.0*(t1 - t0) / (len(uavIds)*(len(varNames) + 1)*N) * 100.0)
             t0 = t1
         for uavId in uavIds:
             gpsx = random.gauss(0.0, gpsSig)
             gpsy = random.gauss(0.0, gpsSig)
             gpsz = random.gauss(0.0, gpsSig)
-            gps  = Gps(uavId+" GPS 3 "+str(int(gpsx))+' '+str(int(gpsy))+' 0 '+str(int(gpsz))+
+            gps  = pmsg.Gps(uavId+" GPS 3 "+str(int(gpsx))+' '+str(int(gpsy))+' 0 '+str(int(gpsz))+
                        ' 0 0 0 0 31 0')
             gps.stamp = n
             dtbase.add_gps(gps)
@@ -54,6 +58,12 @@ try:
                 dtbase.add_sample(sample)
     
 finally:    
+    fig, axes = plt.subplots(1,1)
+    axes.plot(np.linspace(0, (len(uavIds)*(len(varNames) + 1)*N), len(t)) ,t, label="insert time")
+    axes.set_xlabel("database size")
+    axes.set_ylabel("insert time (us)")
+    axes.grid()
+    plt.show(block=False)
     # dtbase.disable_periodic_save()
     dtbase.save('output/database_perf02.neph', True)
 
--- tests/database_perf_load01.py
+++ tests/database_perf_load01.py
@@ -13,14 +13,15 @@ from nephelae_mapping.database import NephelaeDataServer
 
 from helpers.helpers import *
 
-print("loading database... ", end='')
+print("loading database... ", end='', flush=True)
 t0 = time.time()
-dtbase = NephelaeDataServer.load('output/database_perf01.neph')
+# dtbase = NephelaeDataServer.load('output/database_perf01.neph')
+dtbase = NephelaeDataServer.load('output/database_perf02.neph')
 t1 = time.time()
-print("Done. (ellapsed : ", t1 - t0,")")
+print("Done. (ellapsed : ", t1 - t0,")", flush=True)
 
 
-print("Reading database... ")
+print("Reading database... ", flush=True)
 t0 = time.time()
 for i in range(10):
     output = [entry.data for entry in dtbase.find_entries(['GPS','101'],
@@ -28,7 +29,7 @@ for i in range(10):
     # for item in output:
     #      print(item)
 t1 = time.time()
-print("Done. (ellapsed : ", t1 - t0,")")
+print("Done. (ellapsed : ", t1 - t0,")", flush=True)
 
 
 
