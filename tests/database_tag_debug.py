1aea36a2fab85721dbc0f1be0504b2971d4a4bac
Pierre Narvor <pnarvor@laas.fr>
2019-07-17 15:34:53 +0200

    [database] Checked database for far off-bounds access

--- tests/database_tag_debug.py
+++ tests/database_tag_debug.py
@@ -5,6 +5,8 @@ sys.path.append('../../')
 import os
 import signal
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 from ivy.std_api import *
 import logging
@@ -17,9 +19,22 @@ from helpers.helpers import *
 
 database = NephelaeDataServer.load('output/database02.neph')
 
-output = database.find_entries(['101', 'SAMPLE'], Fancy()[:60])
-print("Got", len(output), "entries :") 
-for entry in output:
-    print(entry.data.position.t)
+output = database.find_entries(['101', 'WT'], Fancy()[-3600:], sortCriteria=lambda x: x.data.timeStamp)
+t  = np.array([entry.data.timeStamp for entry in output])
+wt = np.array([entry.data.data[0] for entry in output])
+
+fig, axes = plt.subplots(2,1)
+axes[0].plot(t, label="time stamp")
+axes[0].set_xlabel("Index")
+axes[0].set_ylabel("Time (s)")
+axes[0].legend(loc="lower right")
+axes[0].grid()
+axes[1].plot(t, wt, label="vertical wind")
+axes[1].set_xlabel("Time (s)")
+axes[1].set_ylabel("Wind speed (m/s)")
+axes[1].legend(loc="lower right")
+axes[1].grid()
+
+plt.show(block=False)
 
 
