f64e67a8b38cbf01667154e0efb6e3c4045a10d6
Pierre Narvor <pnarvor@laas.fr>
2019-07-23 12:15:56 +0200

    [mapping] Added an optimal resolution estimation in the kernel type (gives equal to length_scale for RBF)

--- gprmapping/GprKernel.py
+++ gprmapping/GprKernel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.fft as npfft
 import sklearn.gaussian_process.kernels as Kernel
 
 class GprKernel:
@@ -11,8 +12,14 @@ class GprKernel:
     
     """
 
-    def __init__(self, kernel):
+    def __init__(self, kernel, resolutions=None):
         self.kernel = kernel
+        if resolutions is None:
+            self.optimalResolutions = self.compute_optimal_resolutions()
+        else:
+            if len(resolutions) != len(self.kernel.get_params()['length_scale']):
+                raise ValueError("resolutions paramater must the same dimension as the kernel")
+            self.optimalResolutions = resolutions
    
 
     def values(self, locations):
@@ -23,6 +30,44 @@ class GprKernel:
         return np.array(res)
 
 
-    def ideal_resolution(self):
-        pass
+    def compute_optimal_resolutions(self):
+        # N = 4096 # find a criteria ? 
+        lengthScaleFactor = 256
+        # N = 512 # find a criteria ? 
+        # N = 128 * lengthScaleFactor
+        N = 32 * lengthScaleFactor
+        resolutions = []
+
+        lengthScales = self.kernel.get_params()['length_scale']
+        if isinstance(lengthScales, float):
+            lengthScales = [lengthScales]
+
+        for dimIndex, lengthScale in enumerate(lengthScales):
+            samplingRate = N / (2*lengthScaleFactor*lengthScale)
+
+            # Generating a set of locations along the dimension to evaluate
+            # (centered to simplify the fft evaluation
+            locations = np.zeros([N, len(lengthScales)])
+            locations[:,dimIndex] = np.linspace(-lengthScaleFactor*lengthScale,
+                                                 lengthScaleFactor*lengthScale, N)
+
+            # Evaluating the kernel values at these locations
+            # (/!\ not the matrix k(X,X) /!\)
+            kernelValues = self.values(locations)
+
+            # Evaluating optimal resolution in Fourier space (Nyquist-Shannon theorem)
+            ft = np.abs(npfft.fft(kernelValues))
+            ftmin = min([a for a in ft if a > 0])
+            ft[ft < ftmin] = ftmin
+            ft = ft / np.max(ft) # max spectrum value will be 1.0, so 0.0 in db
+            ft = 10*np.log10(ft) # spectrum in db
+
+            # fc = (np.argmax(ft < -20) / N) * samplingRate == frequency where spectrum(kernel) < 20db
+            # So optimal sampling frequency is f0 = 2.0*fc, hence optimal resolution is 1.0 / f0
+            resolutions.append(1.0 / (2.0 * (np.argmax(ft < -20) / N) * samplingRate))
+        return resolutions
+
+
+
+
 
--- tests/mapping02.py
+++ tests/mapping02.py
@@ -11,30 +11,34 @@ import time
 from nephelae_mapping.gprmapping import GprKernel
 from sklearn.gaussian_process import kernels as gpk
 
-N = 512
-span = [-10, 10]
-samplingRate = N / (span[-1] - span[0])
-locations    = np.linspace(span[0], span[-1], N)
+kernel = GprKernel(gpk.RBF(np.array([1.0,2.0,3.0,10.0,100.0])))
+print("Kernel resolutions: ", kernel.optimalResolutions)
 
-kernel = GprKernel(gpk.RBF())
-values = kernel.values(locations.reshape(-1,1))
-
-ft    = np.abs(npfft.fft(values))
-ftmin = min([a for a in ft if a > 0])
-ft[ft < ftmin] = ftmin
-ft = ft / np.max(ft)
-ft = 10*np.log10(ft)
-
-
-fig, axes = plt.subplots(2,1)
-axes[0].plot(locations, values, label="kernel values")
-axes[0].grid()
-axes[0].legend(loc="upper right")
-axes[1].plot(np.linspace(0, samplingRate * (N-1) / N, N), ft, label="kernel fourier")
-axes[1].grid()
-axes[1].legend(loc="upper right")
-
-plt.show(block=False)
+# N = 512
+# span = [-10, 10]
+# samplingRate = N / (span[-1] - span[0])
+# locations    = np.linspace(span[0], span[-1], N)
+# 
+# values = kernel.values(locations.reshape(-1,1))
+# ft    = np.abs(npfft.fft(values))
+# ftmin = min([a for a in ft if a > 0])
+# ft[ft < ftmin] = ftmin
+# ft = ft / np.max(ft)
+# ft = 10*np.log10(ft)
+# 
+# 
+# fig, axes = plt.subplots(2,1)
+# axes[0].plot(locations, values, label="kernel values")
+# axes[0].grid()
+# axes[0].legend(loc="upper right")
+# # for ft in kernel.resolutions:
+#     # axes[1].plot(np.linspace(0,1,len(ft)), ft, '.')
+#     # axes[1].plot(ft, '.')
+# axes[1].plot(np.linspace(0, samplingRate * (N-1) / N, N), ft, label="kernel fourier")
+# axes[1].grid()
+# axes[1].legend(loc="upper right")
+# 
+# plt.show(block=False)
 
 
 
